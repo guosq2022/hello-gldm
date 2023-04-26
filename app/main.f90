80bd09dfa3f5dde961eee57a4006f159d49d42ed
guosq2022 <guosq09@126.com>
2023-04-27 02:41:45 +0800

    测试了(84,216)和(86,216)

--- app/main.f90
+++ app/main.f90
@@ -7,12 +7,12 @@ program main
   call say_hello()
 
   a0 = 212.
-  z0 = 84.
+  z0 = 86. !86. !84.
   a2 = 4.
   z2 = 2.
   a1 = a0 - a2
   z1 = z0 - z2
-  qexp = 8.9542
+  qexp = 6.3847 !6.3847 !8.9542
 
   call gldm(a1, z1, a2, z2, qexp, t12)
 
